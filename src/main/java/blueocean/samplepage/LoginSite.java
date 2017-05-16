5175683a4f1a1299a40439f46f92d0e1e5f1aaf7
Mahesh <devika.mahesh@accenture.com>
2017-05-16 20:44:59 +0530

    blue ocean

---
+++ src/main/java/blueocean/samplepage/LoginSite.java
@@ -0,0 +1,46 @@
+package blueocean.samplepage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+
+public class LoginSite {
+
+	protected WebDriver driver;
+	
+	public LoginSite(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	@FindBy(xpath = "//input[contains(@name,'userName')]")
+	protected WebElement userNameField;
+
+	protected void enterEmail() {
+		userNameField.sendKeys("mercury");
+	}
+	
+	@FindBy(xpath = "//input[contains(@name,'password')]")
+	protected WebElement passwordField;
+
+	protected void enterPassword() {
+		passwordField.sendKeys("mercury");
+	}
+	
+	@FindBy(xpath = "//input[contains(@src,'images')]")
+	protected WebElement signInButton;
+
+	protected void clickSignInButton() {
+		signInButton.click();
+	}
+	
+	
+	public void loginApplication() {
+
+		enterEmail();
+		enterPassword();
+		clickSignInButton();
+		}
+
+}
---
+++ src/test/java/blueocean/sample/AccessToSite.java
@@ -0,0 +1,36 @@
+package blueocean.sample;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import blueocean.samplepage.LoginSite;
+
+
+public class AccessToSite {
+	
+public WebDriver driver;
+	
+	@Test
+	public void flightSite() { 
+		
+		DesiredCapabilities capab = DesiredCapabilities.chrome();
+		capab.setCapability("dom.forms.number", false);
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver(capab);
+
+		driver.get("http://newtours.demoaut.com");
+		
+		System.out.println("Browser is launched");
+		
+		LoginSite loginSite = PageFactory.initElements(driver, LoginSite.class);
+		
+		loginSite.loginApplication();
+		
+		System.out.println("Credentials entered");
+	
+	}
+}
